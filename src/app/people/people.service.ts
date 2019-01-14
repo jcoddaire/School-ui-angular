db79a9740eab6dfa6e6f3f8b69fa53602b5f3409
Jacob Coddaire <Jacob.Coddaire@Spectrumhealth.org>
2019-01-14 16:49:58 -0500

    Added bootstrap and began working on the student data.

--- src/app/people/people.service.ts
+++ src/app/people/people.service.ts
@@ -8,8 +8,11 @@ import { MessageService } from '../messages/message.service'
 import { environment } from '../../environments/environment';
 
 const httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json',
-                                'Access-Control-Allow-Origin': 'http://localhost:4200'})
+    headers: new HttpHeaders ({ 
+        'Content-Type': 'application/json',
+        'method': 'GET'
+        
+    })
 }
 
 @Injectable({
