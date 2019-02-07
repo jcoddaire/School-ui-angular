c6c7f83f252666cea22affd9f83295c2e6aed229
Jacob Coddaire <Jacob.Coddaire@spectrumhealth.org>
2019-02-07 13:17:15 -0500

    Updated to allow the user to add new departments. Need to add description and photo upload, though.

--- src/app/departments/departments.component.ts
+++ src/app/departments/departments.component.ts
@@ -10,12 +10,22 @@ import { MessageService } from '../messages/message.service';
 })
 export class DepartmentsComponent implements OnInit {
 
+  currentName: string;
+  currentBudget: number;
+  currentCreatedDate: Date;
+
   departments: Department[];
 
   constructor(
     private departmentService: DepartmentsService,
     private messageService: MessageService
-    ) { }
+    ) {
+
+      this.currentName = '';
+      this.currentBudget = 0;
+      this.currentCreatedDate = new Date();
+
+     }
 
   ngOnInit() {
     this.getDepartments();
@@ -24,9 +34,35 @@ export class DepartmentsComponent implements OnInit {
   getDepartments(): void {
     this.departmentService.getDepartments().subscribe(d => {
       this.departments = d;
-      // order the departments from highest budget to lowest.
-      this.departments.sort((b, a) => a.budget >= b.budget ? 1 : a.budget === b.budget ? 0 : -1);
+      this.sortDepartmentsByBudget();
     });
+  }
+
+  addDepartment(name: string, budget: number): void {
+
+    if (name === null) {
+      return;
+    }
+
+    if (name.trim() === '') {
+      return;
+    }
+
+    const cDept = new Department();
+    cDept.name = name;
+    cDept.budget = budget;
+    cDept.createdDate = new Date();
+
+    this.departmentService.addDepartment(cDept, this.departments).subscribe(d => {
+      this.departments.push(d);
+      this.sortDepartmentsByBudget();
+    });
+
+  }
+
+  private sortDepartmentsByBudget(): void {
+    // order the departments from highest budget to lowest.
+    this.departments.sort((b, a) => a.budget >= b.budget ? 1 : a.budget === b.budget ? 0 : -1);
   }
 
   private log(message: string) {
--- src/app/departments/departments.service.ts
+++ src/app/departments/departments.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 
 import { Department } from '../models/Department';
 import { environment } from '../../environments/environment';
+import { DepartmentsComponent } from './departments.component';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Access-Control-Allow-Origin': '*'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +21,41 @@ import { environment } from '../../environments/environment';
 export class DepartmentsService {
 
   private ROOT_API_URL = environment.apiUrl + '/api/Department';
+  private DEPARTMENT_ID_STARTING_POINT = 1001;
 
     constructor(private http: HttpClient) {}
 
     getDepartments(): Observable<Department[]> {
 
         return this.http.get<Department[]>(this.ROOT_API_URL);
+    }
+
+    addDepartment(department: Department, departments: Department[]): Observable<Department> {
+
+      if (department && departments) {
+        department.departmentID = this.getUnusedDepartmentID(departments);
+        if (department.departmentID) {
+          return this.http.post<Department>(this.ROOT_API_URL, department, httpOptions);
+        }
+      }
+      return null;
+    }
+
+    //  This logic ought to be on the server (identity column), but I want to see how Angular handles this.
+    getUnusedDepartmentID(departments: Department[]): number {
+
+      let result = 0;
+
+      if (!departments) {
+        return 1;
+      }
+
+      let ii = this.DEPARTMENT_ID_STARTING_POINT;
+      for (ii; ii < Number.MAX_VALUE; ii++) {
+        if (!departments.some(d => d.departmentID === ii)) {
+          return ii;
+        }
+      }
+      return null;
     }
 }
